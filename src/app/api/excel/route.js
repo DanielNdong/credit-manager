166cff49142f6a062216fbc9d1b2e217b70ef535
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2024-03-30 01:33:26 +0100

    Add files via upload

--- src/app/api/excel/route.js
+++ src/app/api/excel/route.js
@@ -1,3 +1,122 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from Next.js!' })
+import { Workbook } from "exceljs";
+import { NextResponse } from "next/server";
+
+const createExcel = async() => {
+  const workbook = new Workbook();
+
+  const worksheet = workbook.addWorksheet("Seguiminiento de créditos");
+  worksheet.columns = [
+    { header: "AGENTE", key: "agent" },
+    { header: "NOMBRE CLIENTE", key: "cli" },
+    { header: "SEXO", key: "sex" },
+    { header: "TIPO PERSONA", key: "tperson" },
+    { header: "COD CLIENTE", key: "cod_cli" },
+    { header: "EMPRESA CLIENTE", key: "comp_cli" },
+    { header: "MONTO", key: "amount" },
+    { header: "FECHA CONCESION", key: "date" },
+    { header: "PERIODO", key: "period" },
+    { header: "SITUACION", key: "state" },
+  ];
+
+   // Estilo para los encabezados
+   const headerStyle = {
+    fill: {
+      type: 'pattern',
+      pattern: 'solid',
+      fgColor: {
+        argb: '808080', // Color gris
+      },
+    },
+  };
+
+  worksheet.columns.forEach((header) => {
+    header.style = headerStyle;
+  })
+
+  await workbook.xlsx.writeFile('./public/files/seguimiento-creditos.xlsx')
+
+};
+
+export async function POST( request, {params} ) {
+  const data =  request.json()
+  const workbook = new Workbook();
+  try {
+    console.log(data)
+    await workbook.xlsx.readFile('./public/files/seguimiento-creditos.xlsx')
+    const worksheet = workbook.getWorksheet('Seguiminiento de créditos')
+
+    const row = worksheet.addRow([...data]);
+    row.font = {
+      name: "Arial",
+      size: 12,
+      bold: true,
+    };
+    await workbook.xlsx.writeFile("./public/files/animes2.xlsx");
+
+    return NextResponse.json({
+      data,
+      worksheet
+    })
+  } catch (error) {
+    console.log('Error: ' + error)  
   }
+}
+export async function GET(request, { params }) {
+  createExcel()
+  const workbook = new Workbook();
+  const filePath = "./src/app/api/excel/mi-archivo2.xlsx";
+
+  const createNewExcelFile = async () => {
+
+    const row = worksheet.addRow(["One Piece", "Oda", "1999"]);
+
+    row.font = {
+      name: "Arial",
+      size: 12,
+      bold: true,
+    };
+
+    worksheet.getCell("A1").dataValidation = {
+      type: "list",
+      allowBlank: true,
+      formulae: ['"One,Two,Three,Four"'],
+    };
+
+    await workbook.xlsx.writeFile("./public/files/animes2.xlsx");
+  };
+
+  const editCellExcelFile = async () => {
+    /*Guardar el excel */
+    try {
+      const workbook = new Workbook();
+      await workbook.xlsx.readFile("./public/files/animes2.xlsx");
+      /* leo la hoja */
+      const worksheet = workbook.getWorksheet("Hoja1");
+
+      let cell = worksheet.getCell("C3");
+      cell.value = "2018";
+      worksheet.getRow(4).values;
+      /*  const row = worksheet.addRow(['Kimetsu no Yaiba', 'Unknow', '2014']);
+      row.font = {
+        name: 'Arial',
+        size: 12,
+        bold: true,
+      }; */
+      await workbook.xlsx.writeFile("./public/files/animes2.xlsx");
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+    /*     worksheet.insertRow(3, {id: 1, name: 'John Doe', dob: new Date(1970,1,1)}); */
+    // Modificar el valor de una celda
+    /* let cells =  worksheet.getCell("A1").value = 'Peliculas';
+   console.log(worksheet) */
+
+    // Agregar una nueva fila
+    /*      worksheet.addRow(['Daniel Aquilino Ndong', 'Bibang Eyang', '24']);    
+     worksheet.addRow(['Juan Andrés', 'Owono', '22']);   */
+  };
+  editCellExcelFile();
+
+  return NextResponse.json({ message: "excel editado" });
+}
+/*     const { searchParams } = new URL(request.url)*/
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,104 +1,141 @@
 'use client'
 
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import inputStyles from './css-modules/Input.module.css'
+import { useForm } from 'react-hook-form';
+import React from 'react';
+import inputStyles from './css-modules/Input.module.css';
 
 
 const Form = () => {
-  const route = useRouter();
+  
+  const {register, handleSubmit, 
+    formState: {errors},
+    watch
+  } = useForm();
 
-    const [values, setValues] = useState({
-      agent: "",
-      clientName: "",
-      sex: "",
-      tpersona: "",
-      cliCode: "",
-      cliCompany: "",
-      amount: "",
-      date: "",
-      period: "",
-      tproduct: "",
-      state: "",
+  const onSubmit = async(data) => {
+    try {
+      const response = await fetch('http://localhost:3000/api/excel', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
     })
-
-
-  const handleInputChange = (event) => {
-    const {id, value} = event.target;
-    console.log(event.target)
-    setValues({
-      ...values,
-      [id]: value
-    })
-  }
-
-  const handleForm = (event) => {
-    event.preventDefault();
-    console.log(values)
-  }
+    const res = response.json()
+    console.log("data: " + res)
+   } catch (error) {
+    console.log(error)
+   }
+  };
 
   return (
-    <form id="idform">
+    <form className='w-auto' id="idform" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex gap-2 border w-max px-4 py-5 ">
           <div className="grid gap-2">
             <label htmlFor="agent">
               <span className="mr-3">Agente</span>
-              <input className={inputStyles.input} type="text" id="agent" onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("agent", {
+                required: true,                
+              })}/>
             </label>
+            {errors.agent && <span className='text-red-400 font-light text-sm'>Nombre del agente requerido</span>}
             <label htmlFor="clientName">
               <span className="mr-3">Cliente</span>
-              <input className={inputStyles.input} id="clientName" onChange={handleInputChange}/>
+              <input type='text' className={inputStyles.input} 
+              {...register("clientName", {
+                required: true,
+                minLength: 2,
+                maxLength: 20
+              })}/>
             </label>
+            {errors.clientName?.type == "minLength" && <span className='text-red-400 font-light text-sm'>La longitud minima es de 2 caracteres</span>}
+            {errors.clientName?.type == "maxLength" && <span className='text-red-400 font-light text-sm'>La longitud maxima es de 20 requerido</span>}
+            {errors.clientName?.type == "required" && <span className='text-red-400 font-light text-sm'>Nombre del cliente requerido</span>}
             <label htmlFor="sex">
               <span className="mr-3">Sexo</span>
-              <input className={inputStyles.input} id="sex" onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("sex", {
+                required: true
+              })}/>
             </label>
-            <label htmlFor="tpersona">
+            {errors.sex && <span className='text-red-400 font-light text-sm'>Tipo de sexo requerido</span>}
+            <label htmlFor="tperson">
               <span className="mr-3">Tipo de persona</span>
-              <input className={inputStyles.input} id='tpersona' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("tperson", {
+                required: true
+              })}/>
             </label>
+            {errors.tperson && <span className='text-red-400 font-light text-sm'>Tipo de Persona requerido</span>}
             <label htmlFor="cliCode">
               <span className="mr-3">Codigo cliente</span>
-              <input className={inputStyles.input} id='cliCode' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("cliCode", {
+                required: true
+              })}/>
             </label>
+            {errors.cliCode && <span className='text-red-400 font-light text-sm'>Código del cliente requerido</span>}
             <label htmlFor="cliCompany">
               <span className="mr-3">Empresa cliente</span>
-              <input className={inputStyles.input} id='cliCompany' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("cliCompany", {
+                required: true
+              })}/>
             </label>
+            {errors.cliCompany && <span className='text-red-400 font-light text-sm'>Empresa a la que pertenece el cliente requerida</span>}
           </div>
           <div className="grid gap-2">
             <label htmlFor="amount">
               <span className="mr-3">Monto</span>
-              <input className={inputStyles.input} id='amount' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("amount", {
+                required: true
+              })}/>
             </label>
+            {errors.amount && <span className='text-red-400 font-light text-sm'>Monto requerido</span>}
             <label htmlFor="date">
               <span className="mr-3">Fecha de concesion</span>
-              <input className={inputStyles.input} id='date' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("date", {
+                required: true
+              })}/>
             </label>
+            {errors.date && <span className='text-red-400 font-light text-sm'>Fecha requerida</span>}
             <label htmlFor="period">
               <span className="mr-3">Periodo</span>
-              <input className={inputStyles.input} id='period' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("period", {
+                required: true
+              })}/>
             </label>
+            {errors.period && <span className='text-red-400 font-light text-sm'>Periodo requerido</span>}
             <label htmlFor="tproduct">
               <span className="mr-3">Tipo de producto</span>
-              <input className={inputStyles.input} id='tproduct' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("tproduct", {
+                required: true
+              })}/>
             </label>
+            {errors.tproduct && <span className='text-red-400 font-light text-sm'>Tipo de producto requerido</span>}
             <label htmlFor="state">
               <span className="mr-3">Situacion</span>
-              <input className={inputStyles.input} id='state' onChange={handleInputChange}/>
+              <input className={inputStyles.input} 
+              {...register("state", {
+                required: true
+              })}/>
             </label>
+            {errors.state && <span className='text-red-400 font-light text-sm'>Estado del credito requerido</span>}
           </div>
         </div>
 
-        <div className="mt-4">
-          <input className="w-max border-2 bg-gray-800 text-white active:bg-slate-500 px-3 py-1" type="button" value="Insertar crédito" id="submitButton"
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(values)
-            /*  route.push(`./${values.agent}&${values.clientName}&${values.sex}&${values.tpersona}&${values.cliCode}&${values.cliCompany}&${values.amount}&${values.date}&${values.period}&${values.tproduct}&${values.state}`)*/
-            route.push('./api/excel/route')
-          }}/>
+        <div className="mt-4 w-min">
+          <input className="w-max border-2 bg-gray-800 text-white active:bg-slate-500 px-3 py-1" type="submit" value="Insertar crédito" id="submitButton"
+          />
         </div>
+          <pre>
+            {JSON.stringify(watch(), null, 2)}
+          </pre>
       </form>
   )
 }
